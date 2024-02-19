6662b80e285290a6d19f805f036082cbae17c5e6
ClementMZ6 <9103@holbertonstudents.com>
2024-02-19 14:57:53 +0100

    .

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#include <stdio.h>int main() {
-    char chaine[] = "Programming is like building a multilingual puzzle";
-
-    puts(chaine); // Affiche la chaîne suivie d'un saut de ligne
-
-    return 0;
+#include <stdio.h>
+int main() {
+	char chaine[]="Programming is like building a multilingual puzzle";
+	puts(chaine);
+	return 0;
 }
