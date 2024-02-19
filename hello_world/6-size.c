1c120f15b302db259b08d2be6e4653c278aef0c0
ClementMZ6 <9103@holbertonstudents.com>
2024-02-19 16:30:18 +0100

    .

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-	char
+#include <stdio.h>
+/**
+ * main - décrire la taille de chaque ligne, -m32
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("Size of a char: %zu 1\n", sizeof(char));
+printf("Size of an int: %zu 4\n", sizeof(int));
+printf("Size of a long int: %zu 4\n", sizeof(long int));
+printf("Size of a long long int: %zu 8\n", sizeof(long long int));
+printf("Size of a float: %zu 4\n", sizeof(float));
+
+return (0);
+}
