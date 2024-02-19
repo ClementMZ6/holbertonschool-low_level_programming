54ca2cdbc3a4b6894e8008070dc39f40c08d3778
ClementMZ6 <9103@holbertonstudents.com>
2024-02-19 16:37:41 +0100

    .

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-printf("Size of a char: %zu 1\n", sizeof(char));
-printf("Size of an int: %zu 4\n", sizeof(int));
-printf("Size of a long int: %zu 4\n", sizeof(long int));
-printf("Size of a long long int: %zu 8\n", sizeof(long long int));
-printf("Size of a float: %zu 4\n", sizeof(float));
+printf("Size of a char: %d 1\n", sizeof(char));
+printf("Size of an int: %d 4\n", sizeof(int));
+printf("Size of a long int: %d 4\n", sizeof(long int));
+printf("Size of a long long int: %d 8\n", sizeof(long long int));
+printf("Size of a float: %d 4\n", sizeof(float));
 
 return (0);
 }
