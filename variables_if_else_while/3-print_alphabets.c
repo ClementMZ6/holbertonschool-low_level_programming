e0136bcba29eb483a10f88da239f03c70664ad6b
ClementMZ6 <9103@holbertonstudents.com>
2024-02-21 10:57:29 +0100

    its starting to sink in

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - /three.c
+ * same of two but add followed uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
