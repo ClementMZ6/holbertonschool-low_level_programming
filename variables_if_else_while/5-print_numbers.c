29585f6b090eda2d1e8b086d1b046794308a1032
ClementMZ6 <9103@holbertonstudents.com>
2024-02-21 12:19:44 +0100

    ez jgl diff

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Numbers
+ * print digit numbers from 0 to 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+	printf("%d\n", i);
+	i++;
+	}
+	return (0);
+}
