67aff04c93895c891b892b029ebe348550c15075
ClementMZ6 <9103@holbertonstudents.com>
2024-02-21 09:58:36 +0100

    2-print finished tanished

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - blak blok
+ *just putchar alphabet
+ *Return: 0
+ */
+int main(void)
+{
+
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	return (0);
+}
