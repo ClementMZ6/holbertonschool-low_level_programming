66b6011345a5d41f6cefdef1e42147349e171b67
ClementMZ6 <9103@holbertonstudents.com>
2024-02-21 12:21:29 +0100

    print digit numbers of base 10 starting from 0

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Numbers
- * print digit numbers from 0 to 10
+ * print digit numbers from base 10 starting 0
  * Return: 0
  */
 int main(void)
 {
 	int i = 0;
 
-	while (i <= 10)
+	while (i <= 9)
 	{
 	printf("%d\n", i);
 	i++;
