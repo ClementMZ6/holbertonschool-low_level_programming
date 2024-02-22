5adebdfcd5c1c91a27b48c2882b7cc84b38045de
ClementMZ6 <9103@holbertonstudents.com>
2024-02-22 15:01:27 +0100

    Prototype: void print_alphabet(void);

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - blowjob
+ * Return: 0 ALWAYS IS SAID
+ */
+void print_alphabet(void)
+{
+	char z = 'a';
+	while (z <= 'z')
+	{
+		_putchar(z);
+		z++;
+	}
+	_putchar('\n');
+}
