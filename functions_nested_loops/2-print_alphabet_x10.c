39196a3b95d214e65ec84de8475807a3e7aeba9d
ClementMZ6 <9103@holbertonstudents.com>
2024-02-22 16:57:07 +0100

    combinated cmd

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - ALWAYS IS SAID
+ */
+void print_alphabet_x10(void)
+{
+	char z = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		z ='a';
+		while (z <= 'z')
+		{
+			_putchar(z);
+			z++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
