3d9349168ea7944f0d389c2782716cdbd55bdb79
ClementMZ6 <9103@holbertonstudents.com>
2024-02-23 13:22:40 +0100

    abstrait value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * abs - project vodka
+ * @abs: absolute value of integer
+ * Return: n if n and -n if -n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return -n;
+	else
+		return n;
+}
