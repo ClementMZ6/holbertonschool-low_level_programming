5c75020d0cc36fc12877d81295a931dab45c0953
ClementMZ6 <9103@holbertonstudents.com>
2024-02-23 13:29:01 +0100

    abstrait value

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
  * print_sign - Sign Of Number.
- * @n: symbols ~
- * Returns: 1 and prints + if n is greater than zero
- * Returns: 0 and prints 0 if n is zero
- * Returns: -1 and prints - if n is less than zero
+ * @n: symbols
+ * Return: 1 or 0 or -1
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * abs - project vodka
- * @abs: absolute value of integer
+ * _abs - absolute value of an integer.
+ * @n: number
  * Return: n if n and -n if -n
  */
 int _abs(int n)
 {
 	if (n < 0)
-		return -n;
+		return (-n);
 	else
-		return n;
+		return (n);
 }
