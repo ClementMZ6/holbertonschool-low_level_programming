a3620b927186d7127386cc16b18af2743d813655
ClementMZ6 <9103@holbertonstudents.com>
2024-02-23 10:49:05 +0100

    go speed run that

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha : is a letter
+ * @c: letter
+ * Return: 1 if 'c' is a letter, lowercase or uppercase, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c <= 122 && c >= 97) || (c <= 90 && c >= 65))
+		return (1);
+	else
+		return (0);
+}
